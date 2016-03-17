77c997d68948ef7c0f9cf2f0177dd05ff0c55e5b
Kristen Mills <kristen@kristen-mills.com>
2016-03-17 11:23:03 -0700

    Installs more things

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ git submodule update
 
 dotfiles=`pwd`
 
+mkdir $HOME/.config
+
 ln -s $dotfiles/oh-my-zsh-custom/ $HOME/.oh-my-zsh/custom
 ln -s $dotfiles/powerline-config/ $HOME/.config/powerline
 ln -s $dotfiles/vim $HOME/.vim
@@ -13,3 +15,9 @@ ln -s $dotfiles/gitignore_global $HOME/.gitignore_global
 ln -s $dotfiles/tmux.conf $HOME/.tmux.conf
 ln -s $dotfiles/vimrc $HOME/.vimrc
 ln -s $dotfiles/zshrc $HOME/.zshrc
+
+git clone https://github.com/jimeh/tmuxifier.git $HOME/.tmuxifier
+git clone https://github.com/chriskempson/base16-shell $HOME/.config/base16-shell
+
+brew install python fortune cowsay
+gem install lolcat
