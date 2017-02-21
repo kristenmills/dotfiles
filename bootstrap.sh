eb32179bec6b9e9e614e9b9b9a9b5ac586d8e14f
Kristen Mills <kristen@kristen-mills.com>
2017-02-21 11:12:38 -0800

    Cleaned up bootstrap and added zshrc overrides

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env zsh
 
+function moveAndLink() {
+  if [ -e $1 ] && [ ! -h $1 ]; then
+    echo "mv $1 $1.old"
+    mv $1 $1.old
+  fi
+
+  if [ ! -e $1 ]; then
+    echo "ln -s $2 $1"
+    ln  -s $2 $1
+  fi
+}
+
+function installOrUpdate() {
+   if [ ! -d $1 ]; then
+     echo "git clone $2 $1"
+     git clone $2 $1
+   else
+     echo "Updating $1"
+     cd $1
+     git pull
+     cd $dotfiles
+   fi
+}
+
 git submodule init
 git submodule update
 
@@ -10,92 +34,60 @@ mkdir -p $HOME/.config
 mkdir -p $dotfiles/vim/undo
 mkdir -p $HOME/development
 
+if [ ! -e $HOME/.zshrc.overrides ]; then
+  echo "Creating Overrides file"
+  echo "# For things I don't want to accidently commit" > $HOME/.zshrc.overrides
+fi
+
 # Install Oh-my-zsh
 if [ ! -d $HOME/.oh-my-zsh ]; then
+  echo "Installing Oh My Zsh. Run bootstrap again with this finishes"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 # link all the things
+echo "Linking custom zsh files"
 ln -s $dotfiles/oh-my-zsh-custom/* $HOME/.oh-my-zsh/custom
-if [ ! -d $HOME/.config/powerline ]; then
-  ln -s $dotfiles/powerline-config/ $HOME/.config/powerline
-fi
-if [ ! -d $HOME/.vim ]; then
-  ln -s $dotfiles/vim $HOME/.vim
-fi
-if [ ! -d $HOME/.config/nvim ]; then
-  ln -s $dotfiles/vim $HOME/.config/nvim
-fi
-if [ -e $HOME/.gitconfig ] && [ ! -h $HOME/.gitconfig ]; then
-  mv $HOME/.gitconfig $HOME/.gitconfig.old
-  ln -s $dotfiles/gitconfig $HOME/.gitconfig
-fi
-if [ -e $HOME/.gitignore_global ] && [ ! -h $HOME/.gitignore_global ]; then
-  mv $HOME/.gitignore_global $HOME/.gitignore_global.old
-  ln -s $dotfiles/gitignore_global $HOME/.gitignore_global
-fi
-if [ -e $HOME/.tmux.conf ] && [ ! -h $HOME/.tmux.conf ]; then
-  mv $HOME/.tmux.conf $HOME/.tmux.conf
-  ln -s $dotfiles/tmux.conf $HOME/.tmux.conf
-fi
-if [ -e $HOME/.vimrc ] && [ ! -h $HOME/.vimrc ]; then
-  mv $HOME/.vimrc $HOME/.vimrc.old
-  ln -s $dotfiles/vimrc $HOME/.vimrc
-fi
-if [ -e $HOME/.zshrc ] && [ ! -h $HOME/.zshrc ]; then
-  mv $HOME/.zshrc $HOME/.zshrc.old
-  ln -s $dotfiles/zshrc $HOME/.zshrc
+
+moveAndLink $HOME/.config/powerline $dotfiles/powerline-config
+moveAndLink $HOME/.vim $dotfiles/vim
+moveAndLink $HOME/.config/nvim $dotfile/vim
+moveAndLink $HOME/.gitconfig $dotfiles/gitconfig
+moveAndLink $HOME/.gitignore_global $dotfiles/gitignore_global
+moveAndLink $HOME/.tmux.conf $dotfiles/tmux.conf
+moveAndLink $HOME/.vimrc $dotfiles/vimrc
+moveAndLink $HOME/.zshrc $dotfiles/zshrc
+
+# install or update all the things
+installOrUpdate $HOME/.tmuxifier https://github.com/jimeh/tmuxifier
+installOrUpdate $HOME/.config/base16-shell https://github.com/chriskempson/base16-shell
+installOrUpdate $HOME/.config/fonts https://github.com/powerline/fonts
+$HOME/.config/fonts/install.sh
+
+# Install or update  Homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+  echo "Installing Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Updating Homebrew"
+  brew update
 fi
 
-# Install or update tmuxifier
-if [ ! -d $HOME/.tmuxifier ]; then
-  git clone https://github.com/jimeh/tmuxifier.git $HOME/.tmuxifier
-else
-  cd $HOME/.tmuxifier
-  git pull
-  cd $dotfiles
-fi
-
-# Install or update powerline fonts
-if [ ! -d $HOME/.config/fonts ]; then
-  git clone https://github.com/powerline/fonts $HOME/.config/fonts
-  $HOME/.config/fonts/install.sh
-else
-  cd $HOME/.config/fonts
-  git pull
-  $HOME/.config/fonts/install.sh
-  cd $dotfiles
-fi
-
-# Install or update base16
-if [ ! -d $HOME/.config/base16-shell ]; then
-  git clone https://github.com/chriskempson/base16-shell $HOME/.config/base16-shell
-else
-  cd $HOME/.config/base16-shell
-  git pull
-  cd $dotfiles
-fi
+# brew install all the things
+brew install python3 autojump python fortune cowsay rbenv ruby-build tmux
 
 # Install Ruby and lolcat
 if [ ! -d $HOME/.rbenv/versions/$ruby_version ]; then
+  echo "Installing ruby $ruby_version"
   rbenv install $ruby_version
   rbenv global $ruby_version
   eval "$(rbenv init -)"
 fi
 gem install lolcat
 
-# Install or update  Homebrew
-which -s brew
-if [[ $? != 0 ]] ; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-    brew update
-fi
-
-# brew install all the things
-brew install python3 autojump python fortune cowsay rbenv ruby-build tmux
-
 # Install neovim
+echo "Installing Neovim"
 brew install neovim/neovim/neovim
 pip3 install neovim
 
--- zshrc
+++ zshrc
@@ -82,3 +82,5 @@ BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 
 fortune | cowsay -f stegosaurus | lolcat
+
+source $HOME/.zshrc.overrides
